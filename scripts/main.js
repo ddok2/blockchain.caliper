f05d69cb57ed22aade21838a7907d84b3d8c5680
SUNGYUB NA <darkerkorean@gmail.com>
2019-05-03 09:56:22 +0900

    add nuri config files

--- scripts/main.js
+++ scripts/main.js
@@ -14,48 +14,53 @@ var exec = require('child_process').exec;
 const Util = require('../src/comm/util');
 
 async function main() {
-    let program = require('commander');
-    program
-        .allowUnknownOption()
-        .option('-c, --config <file>', 'config file of the benchmark')
-        .option('-n, --network <file>', 'config file of the blockchain system under test')
-        .parse(process.argv);
-
+    // let program = require('commander');
+    // program
+    //     .allowUnknownOption()
+    //     .option('-c, --config <file>', 'config file of the benchmark')
+    //     .option('-n, --network <file>', 'config file of the blockchain system under test')
+    //     .parse(process.argv);
+    //
     let logger = Util.getLogger('scripts/main.js');
-    let absConfigFile;
-    if(typeof program.config === 'undefined') {
-      logger.error('config file is required');
-      return;
-    }
-    else {
-        absConfigFile = path.isAbsolute(program.config) ? program.config : path.join(__dirname, '/../', program.config);
-    }
-    if(!fs.existsSync(absConfigFile)) {
-        logger.error('file ' + absConfigFile + ' does not exist');
-        return;
-    }
+    // let absConfigFile;
+    // if(typeof program.config === 'undefined') {
+    //   logger.error('config file is required');
+    //   return;
+    // }
+    // else {
+    //     absConfigFile = path.isAbsolute(program.config) ? program.config : path.join(__dirname, '/../', program.config);
+    // }
+    // if(!fs.existsSync(absConfigFile)) {
+    //     logger.error('file ' + absConfigFile + ' does not exist');
+    //     return;
+    // }
+    //
+    // let absNetworkFile;
+    // if(typeof program.network === 'undefined') {
+    //     logger.error('network file is required');
+    //     return;
+    // }
+    // else {
+    //     absNetworkFile = path.isAbsolute(program.network) ? program.network : path.join(__dirname, '/../', program.network);
+    // }
+    // if(!fs.existsSync(absNetworkFile)) {
+    //     logger.error('file ' + absNetworkFile + ' does not exist');
+    //     return;
+    // }
 
-    let absNetworkFile;
-    if(typeof program.network === 'undefined') {
-        logger.error('network file is required');
-        return;
-    }
-    else {
-        absNetworkFile = path.isAbsolute(program.network) ? program.network : path.join(__dirname, '/../', program.network);
-    }
-    if(!fs.existsSync(absNetworkFile)) {
-        logger.error('file ' + absNetworkFile + ' does not exist');
-        return;
-    }
+    process.env.OVERWRITE_GOPATH = false;
+
+    let absConfigFile = path.join(__dirname, '../nuri/config.yaml');
+    let absNetworkFile = path.join(__dirname, '../nuri/fabric-go-tls.json');
 
     const framework = require('../src/comm/bench-flow.js');
-    try { 
-        await framework.run(absConfigFile, absNetworkFile); 
+    try {
+        await framework.run(absConfigFile, absNetworkFile);
         logger.info('Benchmark run successfully');
-        process.exit(0); 
-    } catch (err) { 
-        logger.error(`Error while executing the benchmark: ${err.stack ? err.stack : err}`); 
-        process.exit(1); 
+        process.exit(0);
+    } catch (err) {
+        logger.error(`Error while executing the benchmark: ${err.stack ? err.stack : err}`);
+        process.exit(1);
     }
 }
 
